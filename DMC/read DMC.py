3c2fe9a6d3661c968d033f4f8d3eda85ca8930d8
daniel muncaciu <danieldfgro2000@gmail.com>
2020-05-23 23:52:54 +0300

    dfg

--- DMC/read DMC.py
+++ DMC/read DMC.py
@@ -4,12 +4,12 @@ import pyzbar.pyzbar as pyzbar
 
 # decode from img
 
-# img = cv2.imread("../QR_code")
-#
-# decodedObjects = pyzbar.decode(img)
-# print(decodedObjects)
-#
-# cv2.imshow("Image", img)
-# cv2.waitKey(0)
+img = cv2.imread("../QR_code")
+
+decodedObjects = pyzbar.decode(img)
+print(decodedObjects)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
 
 # decode from video
